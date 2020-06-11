a9631f59570bfca8bdd5ad8b36737022281a1805
Ben Loney <benloney@gmail.com>
2020-06-10 19:40:06 -0500

    code cleanup

--- public/api.js
+++ public/api.js
@@ -28,12 +28,7 @@ const API = {
     const json = await res.json();
 
     return json;
-  },
+  }
 
-  async getWorkoutsInRange() {
-    const res = await fetch(`/api/workouts/range`);
-    const json = await res.json();
-
-    return json;
-  },
+ 
 };
