abea0dea2247ce29edac7b702875eec638bb2719
Ben Loney <benloney@gmail.com>
2020-06-13 11:54:25 -0500

    edits

--- routes/view.js
+++ routes/view.js
@@ -5,7 +5,6 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
