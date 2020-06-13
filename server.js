775a5b7c6d8c65f8bdd496e9b2e5f34dafc3a6db
Ben Loney <benloney@gmail.com>
2020-06-13 11:13:43 -0500

    uncommented

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
-// const logger = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
-// const db = require("./models");
+const db = require("./models");
 
 const app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
