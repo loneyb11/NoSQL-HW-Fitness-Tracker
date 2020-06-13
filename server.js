ce8ded9124ef3a72774bc26a3ab53692909b5b6a
Ben Loney <benloney@gmail.com>
2020-06-13 08:42:00 -0500

    MONGODB_URI

--- server.js
+++ server.js
@@ -13,14 +13,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
